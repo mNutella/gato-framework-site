28760ee45801de78ab3428fdf43cbccd8afe9ac6
nutella <netcore.click@gmail.com>
2023-06-10 11:22:24 -0400

    feat: add custom gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-radial': 'radial-gradient(50% 50% at 50% 50%, rgba(226, 232, 240, 0.1) 0%, rgba(226, 232, 240, 0) 100%);',
       },
     },
   },
