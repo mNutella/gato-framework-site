a3072b8c6260c0a134125fac326e0fbd933cedc1
nutella <netcore.click@gmail.com>
2023-06-10 11:24:45 -0400

    feat: add nav data

---
+++ src/lib/data/nav.ts
@@ -0,0 +1,34 @@
+export const NAV_ITEMS = [
+  {
+    id: 1,
+    title: "Learn",
+    items: [
+      { id: 1, name: "Read Proposal", href: "#" },
+      { id: 2, name: "Getting Started", href: "#" },
+      { id: 3, name: "Introduction", href: "#" },
+      { id: 4, name: "Traditions", href: "#" },
+      { id: 5, name: "Literature", href: "#" },
+      { id: 6, name: "Learning Resources", href: "#" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Explore",
+    items: [
+      { id: 1, name: "Last News", href: "#" },
+      { id: 2, name: "Ecosystem", href: "#" },
+      { id: 3, name: "Showcases", href: "#" },
+      { id: 4, name: "Blog", href: "#" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Join",
+    items: [
+      { id: 1, name: "Join", href: "#" },
+      { id: 2, name: "Discords and more", href: "#" },
+      { id: 3, name: "Working Groups", href: "#" },
+      { id: 4, name: "Events", href: "#" },
+    ],
+  },
+];
