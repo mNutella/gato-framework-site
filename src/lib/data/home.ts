d8ded2ff3179ebea2e91d920020e5356e9194ded
nutella <netcore.click@gmail.com>
2023-06-10 02:30:51 -0400

    feat: implement home page

---
+++ src/lib/data/home.ts
@@ -0,0 +1,152 @@
+export const NEWS = [
+  {
+    id: 1,
+    href: "/learn",
+    company: "OPEN AI",
+    date: "27 MAY",
+    title: "Democratic Inputs to AI",
+  },
+  {
+    id: 2,
+    href: "/learn",
+    company: "OPEN AI",
+    date: "27 MAY",
+    title: "Democratic Inputs to AI",
+  },
+  {
+    id: 3,
+    href: "/learn",
+    company: "OPEN AI",
+    date: "27 MAY",
+    title: "Democratic Inputs to AI",
+  },
+  {
+    id: 4,
+    href: "/learn",
+    company: "OPEN AI",
+    date: "27 MAY",
+    title: "Democratic Inputs to AI",
+  },
+];
+
+export const GLOBAL_COOPERATION = [
+  {
+    id: 1,
+    title: "A Path to Utopia",
+    items: [
+      {
+        id: 1,
+        title: "The Emerging Importance of AI Alignment and Control",
+        description:
+          "Highlighting the significant role of artificial intelligence in our lives, and the need for these systems to align with human values and ethical principles.",
+      },
+      {
+        id: 2,
+        title: "Introducing GATO",
+        description:
+          "Unveiling the Global Alignment Taxonomy Omnibus, a comprehensive framework designed to solve AI alignment and control issues.",
+      },
+      {
+        id: 3,
+        title: "A Multidimensional Approach",
+        description:
+          "Showcasing GATO's unique multi-layered approach, encompassing everything from model alignment and system architecture to corporate policies and international agreements.",
+      },
+      {
+        id: 4,
+        title: "The Power of Global Cooperation",
+        description:
+          "Promoting GATO's commitment to fostering global consensus and cooperation in addressing AI challenges.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Our Mission",
+    items: [
+      {
+        id: 1,
+        title: "Decentralized Approach",
+        description:
+          "Underlining our mission to create a fully decentralized and federated method to solve AI alignment and control problems.",
+      },
+      {
+        id: 2,
+        title: "Easy Access and Dissemination",
+        description:
+          "Highlighting our commitment to making the GATO Framework easily understandable, adoptable, and sharable through the provision of documents, data, code, and multimedia.",
+      },
+      {
+        id: 3,
+        title: "Ethical AI Systems",
+        description:
+          "Stressing our goal to ensure AI systems remain ethically grounded, transparent, and controllable.",
+      },
+      {
+        id: 4,
+        title: "A Brighter Future",
+        description:
+          "Promoting our vision of ushering in a new era of peace, stability, abundance, and prosperity for all through addressing AI alignment and control issues.",
+      },
+    ],
+  },
+];
+
+export const GATO_LAYERS = [
+  {
+    id: 1,
+    title: "Model Alignment",
+    description:
+      "Focuses on individual AI model alignment with human values and ethical principles. This layer includes fine-tuning, handling mesa-optimization, incorporating ethical guidelines, and implementing AI safety measures like reward modeling and distributional reinforcement learning. It aims to ensure that models are designed to be intrinsically aligned with human values.",
+    href: "/learn",
+    image: "/images/home/gato_layers_section_model_alignment.png",
+  },
+  {
+    id: 2,
+    title: "Autonomous Systems",
+    description:
+      "Deals with architectural patterns, cognitive architectures, and system design. It encompasses system robustness, security, resilience, modularity, and interoperability. At this level, emphasis is placed on creating AI systems that work synergistically to maintain alignment, share insights and learnings, and provide a coherent and cohesive response to various challenges.",
+    href: "/learn",
+    image: "/images/home/gato_layers_section_autonomous_systems.png",
+  },
+  {
+    id: 3,
+    title: "Decentralized Network",
+    description:
+      "Focuses on networked systems like federated systems, blockchain, and decentralized autonomous organizations (DAOs). This layer emphasizes algorithmic consensus and oversight, ensuring that networks of AI systems can work together effectively and maintain alignment across multiple nodes.",
+    href: "/learn",
+    image: "/images/home/gato_layers_section_decentralized_network.png",
+  },
+  {
+    id: 4,
+    title: "Corporate Adoption",
+    description:
+      "Addresses organizational responsibility, governance, and oversight in the development and deployment of AI systems. It includes the creation of AI ethics committees, responsible AI practices, and internal policies that guide the company's approach to AI development.",
+    href: "/learn",
+    image: "/images/home/gato_layers_section_corporate_adoption.png",
+  },
+  {
+    id: 5,
+    title: "National Regulation",
+    description:
+      "Involves the establishment of national guidelines, laws, and regulations that govern AI development, deployment, and usage. This layer ensures that AI systems align with societal values and expectations while promoting responsible innovation and competitiveness.",
+    href: "/learn",
+    image: "/images/home/gato_layers_section_national_regulation.png",
+  },
+  {
+    id: 6,
+    title: "International Treaty",
+    description:
+      "Focuses on the creation of international agreements and treaties that set global standards for AI development, safety, and ethical considerations. This layer aims to foster cooperation among countries and ensure that AI technologies are developed in a manner that benefits humanity as a whole.",
+    href: "/learn",
+    image: "/images/home/gato_layers_section_international_treaty.png",
+  },
+  {
+    id: 7,
+    title: "Global Consensus",
+    description:
+      "Represents the highest level of alignment, where global consensus on AI ethics, values, and principles is achieved. This layer emphasizes the importance of continuous dialogue, collaboration, and learning among diverse stakeholders to ensure AI technologies remain beneficial and aligned with human values.",
+    href: "/learn",
+    image: "/images/home/gato_layers_section_global_consensus.png",
+  },
+];
