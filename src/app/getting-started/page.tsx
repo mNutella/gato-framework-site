e0340403e8f409e1b9c2aa44d85e3b9e8bf9ee28
nutella <netcore.click@gmail.com>
2023-06-12 16:56:49 -0400

    feat: implement getting-started page

---
+++ src/app/getting-started/page.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+
+import Contributing from "@/components/Contributing";
+import ReadProposal from "@/components/ReadProposal";
+import NumberedList from "@/components/NumberedList";
+import { PERSONS, GET_INVOLVED } from "@/lib/data/getting-started";
+
+export default function GettingStarted() {
+  return (
+    <>
+      <section className="bg-neutral-900 border border-neutral-800 rounded-[32px]">
+        <div className="flex flex-col gap-6 p-6 lg:grid lg:grid-cols-2 lg:p-16">
+          <div className="space-y-6 flex flex-col justify-center">
+            <div className="space-y-2">
+              {/* <span className="text-xl font-light text-neutral-400"> */}
+              {/*   AI Coordination Strategy */}
+              {/* </span> */}
+              <h1 className="text-3xl lg:text-6xl">Getting Started</h1>
+            </div>
+            <p className="text-lg font-light text-neutral-300 lg:max-w-lg">
+              Welcome to GATO, a global, decentralized movement to advance the
+              principles of axiomatic alignment in AI.
+            </p>
+            <Link href="getting-started/#get-involved" className="btn-primary">
+              Get Involved
+            </Link>
+          </div>
+          <div className="flex flex-col space-y-6">
+            <div className="p-6 bg-neutral-800 rounded-3xl border border-neutral-700 ">
+              <p className="text-lg font-light text-neutral-300">
+                We understand that the GATO Framework is a huge, as is the
+                problem of AI alignment. Be patient and give yourself time to
+                wrap your mind around this.{" "}
+                <strong className="font-bold">
+                  There&apos;s no requirement for a tech background.
+                </strong>
+              </p>
+            </div>
+            <ul className="flex flex-col sm:grid sm:grid-cols-3 gap-3 justify-items-center items-center">
+              {PERSONS.map((item, index) => (
+                <li
+                  key={"person-" + index}
+                  className="text-lg font-light text-neutral-300"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <NumberedList title="Ways to Get Involved" items={GET_INVOLVED} />
+
+      <ReadProposal />
+
+      <Contributing />
+    </>
+  );
+}
---
+++ src/lib/data/getting-started.ts
@@ -0,0 +1,44 @@
+export const PERSONS = [
+  "Communicators",
+  "Teachers",
+  "Educators",
+  "Academics",
+  "Politicians",
+  "Lawyers",
+  "Artists",
+  "...",
+];
+
+export const GET_INVOLVED = [
+  {
+    id: 1,
+    title: "Download and Implement the GATO Framework",
+    description:
+      "Kickstart your journey with GATO by downloading the Framework. Read it, share it, and implement it in your own way. As a decentralized movement, you don't need our permission. Your first step can be as simple as sharing our vision with your network.",
+    hrefText: "Download ->",
+    href: "#",
+  },
+  {
+    id: 2,
+    title: "Apply to Join the Main GATO Group",
+    description:
+      "Ready to commit more? Consider applying to join the main GATO group. We review every application, but please be aware it takes time, and even if you're a valuable contributor, we might not have a fit for you in the main group yet. Stay tuned - public GATO groups are coming soon!",
+    hrefText: "Join ->",
+    href: "#",
+  },
+  {
+    id: 3,
+    title: "Start Your Own GATO Group",
+    description: `You can form your own local GATO group. Check out Chapter 14 "Building Your GATO Community" in the main GATO Framework for guidelines. Gather like-minded individuals and make a real-world impact. You can create local Meetup groups or online communities, such as on Discord, Reddit, and other social platforms.`,
+    hrefText: "",
+    href: "",
+  },
+  {
+    id: 4,
+    title: "Find an Existing GATO Group",
+    description:
+      "Search platforms like Meetup.com for local chapters of GATO. There are also plenty of online AI alignment communities you can join. While they are not explicitly GATO-aligned, we all share the same mission.",
+    hrefText: "Find ->",
+    href: "#",
+  },
+];
