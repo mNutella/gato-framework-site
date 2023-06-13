148016b9d247a17bc21639c49395ac93e9cb8bfe
nutella <netcore.click@gmail.com>
2023-06-12 22:03:55 -0400

    feat: implement traditions page

---
+++ src/app/traditions/page.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import Community from "@/components/Community";
+import Contributing from "@/components/Contributing";
+import MainList from "@/components/MainList";
+import NumberedList from "@/components/NumberedList";
+
+import { GUIDING_TRADITIONS, MAIN_STEPS } from "@/lib/data/traditions";
+
+export default function Traditions() {
+  return (
+    <>
+      <section className="bg-neutral-900 border border-neutral-800 rounded-[32px]">
+        <div className="flex flex-col gap-6 p-6 lg:grid lg:grid-cols-2 lg:p-16">
+          <div className="space-y-6 flex flex-col justify-center order-2 lg:order-1">
+            <div className="space-y-2">
+              {/* <span className="text-xl font-light text-neutral-400"> */}
+              {/*   AI Coordination Strategy */}
+              {/* </span> */}
+              <h1 className="text-3xl lg:text-6xl">GATO Traditions</h1>
+            </div>
+            <p className="text-lg font-light text-neutral-300 lg:max-w-lg">
+              The traditions serve as the guiding principles, the code of
+              conduct that binds us all in our shared mission.
+            </p>
+            <Link href="traditions/#guiding-traditions" className="btn-primary">
+              Explore Traditions
+            </Link>
+          </div>
+
+          <div className="flex relative order-1 lg:order-2">
+            <div className="relative w-full h-[180px] md:min-h-[380px] overflow-hidden rounded-3xl">
+              <Image
+                className="object-cover"
+                src="/images/traditions/gato_traditions.png"
+                alt="World Map"
+                sizes=""
+                fill
+                priority
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <MainList
+        id="guiding-traditions"
+        className="pt-16 md:pt-32"
+        title="Guiding Traditions of GATO"
+        description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+        items={GUIDING_TRADITIONS}
+      />
+
+      <Community />
+
+      <NumberedList
+        className="pb-16 md:pb-32"
+        title="Ways to Get Involved"
+        description="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+        items={MAIN_STEPS}
+      />
+
+      <Contributing />
+    </>
+  );
+}
---
+++ src/lib/data/traditions.ts
@@ -0,0 +1,93 @@
+export const GUIDING_TRADITIONS = [
+  {
+    id: 1,
+    title: "Navigating the AI Alignment Labyrinth",
+    description:
+      "As we navigate the intricate labyrinth of AI alignment, a guiding light emerges in the form of the GATO Traditions. These traditions, akin to the compass points of our endeavor, were born out of the distilled wisdom of established principles from decentralized and leaderless organizations. Our inspirations are as diverse as they are profound, ranging from the transformative ethos of twelve-step programs to the radical inclusivity of Burning Man and the grassroots empowerment of the Occupy movement.",
+    image: "/images/traditions/guiding_traditions_navigating_labyrinth.png",
+  },
+  {
+    id: 2,
+    title: "Inspirations Behind the Traditions",
+    description:
+      "In the grand tapestry of GATO, these traditions thread a common narrative, creating a pattern of behavior, ethics, and aspirations that help us address the formidable global coordination problem. They are not simply guidelines, but the fundamental pillars that uphold our collective efforts towards axiomatic alignment. Like the constitution of a nation, the traditions form the bedrock of our communal ethos, the shared social contract that we, as participants in GATO, pledge to uphold.",
+    image: "/images/traditions/guiding_traditions_inspirations_traditions.png",
+  },
+  {
+    id: 3,
+    title: "Traditions as Pillars of GATO",
+    description:
+      "Each tradition is a commitment, a promise we make to ourselves and to each other. They encapsulate the spirit of starting where we are, working towards consensus, broadcasting our findings, thinking globally while acting locally, maintaining an unwavering commitment, stepping up when needed, and leveraging exponential thinking. They instill in us a sense of purpose, a dedication to the mission, and an ethos of collaboration that transcends borders and boundaries.",
+    image: "/images/traditions/guiding_traditions_pillars_gato.png",
+  },
+  {
+    id: 4,
+    title: "A Shared Social Contract",
+    description:
+      "By adhering to these traditions, we create a shared lexicon, a common rhythm that synchronizes our individual efforts into a harmonious symphony of progress. They serve as the guiding stars in our journey, the enduring principles that illuminate our path towards a future where AI and humanity coexist in a state of aligned prosperity. As you delve into the essence of each tradition, let them inspire you, guide you, and become the core constitution of your participation in GATO.",
+    image: "/images/traditions/guiding_traditions_shared_contract.png",
+  },
+];
+
+export const MAIN_STEPS = [
+  {
+    id: 1,
+    title: "Start where you are, use what you have, do what you can",
+    description:
+      "This first tradition invites every participant, regardless of their background or resources, to contribute to the mission. It underscores the belief that every voice matters, every effort counts, and everyone has something to bring to the table. It's a call to start with your current abilities and knowledge, and utilize the resources at your disposal to contribute to the cause in whatever capacity possible.",
+  },
+  {
+    id: 2,
+    title: "Work towards consensus",
+    description:
+      "This tradition emphasizes the power of collective wisdom and diverse perspectives in decision-making. It's a commitment to deliberation, to finding common ground, and to building a future that reflects a shared vision. It encourages us to constantly seek input from a broad spectrum of stakeholders, fostering an inclusive and democratic approach to AI alignment.",
+  },
+  {
+    id: 3,
+    title: "Broadcast your findings",
+    description:
+      "Open communication and knowledge sharing are the keystones of this tradition. It urges us to share our discoveries, learnings, and insights openly, contributing to a collective knowledge base that benefits all. It fosters a culture of transparency, open collaboration, and mutual learning, crucial for the advancement of AI alignment.",
+  },
+  {
+    id: 4,
+    title: "Think globally, act locally",
+    description:
+      "This tradition embodies the idea that global change starts with local action. It encourages us to understand the larger implications of our work, but also to recognize the power of acting within our immediate environment. It's about making a difference where we can, knowing that each local action contributes to the global goal of AI alignment.",
+  },
+  {
+    id: 5,
+    title: "In it to win it",
+    description:
+      "This tradition is a testament to our unwavering commitment to the mission of AI alignment. It's not just about participation, but about dedication, perseverance, and a steadfast focus on the ultimate goal. It embodies the spirit of resilience, the determination to overcome challenges, and the resolve to see the mission through to its end.",
+  },
+  {
+    id: 6,
+    title: "Step up",
+    description:
+      "This tradition is an empowering call to action. It urges every individual to seize the initiative, take responsibility, and step into leadership roles when necessary. It's about fostering a proactive culture where individuals are ready to take charge, make decisions, and guide the collective effort towards AI alignment.",
+  },
+  {
+    id: 7,
+    title: "Think exponentially",
+    description:
+      "This tradition invites us to harness the power of exponential technologies and network effects. It encourages us to think big, to envision a future marked by exponential growth and progress, and to leverage the power of networks and technology to accelerate our journey towards AI alignment.",
+  },
+  {
+    id: 8,
+    title: "Trust the process",
+    description:
+      "This tradition emphasizes faith in the collective wisdom of the GATO Framework and its strategic layers. It acknowledges that while individual steps may sometimes seem unclear or counterintuitive, there is a broader plan in action, guided by the heuristic imperatives. Trusting the process means having patience, understanding that progress may take time, and believing in the power of collective action towards achieving axiomatic alignment.",
+  },
+  {
+    id: 9,
+    title: "Strike while the iron is hot",
+    description:
+      "This principle encourages proactive action when opportunities for progress arise. It signifies the importance of momentum in pushing the AI alignment mission forward. When conditions are ripe for a breakthrough, swift and decisive action could yield significant progress. This tradition underscores the need for awareness, readiness, and agility within the GATO community to seize these moments and drive forward the collective goal of AI alignment.",
+  },
+  {
+    id: 10,
+    title: "Divide and conquer",
+    description:
+      "Global AI alignment is simply too big for any one person or group to fully encompass. Instead, we must pick apart this problem into numerous, smaller, easier-to-attack problems. Like ants swarming over a predator to defend the colony, our collective efforts to divide and conquer will come to fruition over time.",
+  },
+];
