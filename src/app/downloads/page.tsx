ff17adbfcfb7d1020ccd4345a331cbc86c5b395e
nutella <netcore.click@gmail.com>
2023-06-12 23:10:25 -0400

    feat: implement downloads page

---
+++ src/app/downloads/page.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+
+import { DOWNLOADS } from "@/lib/data/downloads";
+
+export default function page() {
+  return (
+    <>
+      <section className="bg-neutral-900 border border-neutral-800 rounded-[32px]">
+        <div className="flex flex-col gap-6 p-6 lg:grid lg:grid-cols-2 lg:p-16">
+          <div className="space-y-6 flex flex-col justify-center">
+            <div className="space-y-2">
+              {/* <span className="text-xl font-light text-neutral-400"> */}
+              {/*   AI Coordination Strategy */}
+              {/* </span> */}
+              <h1 className="text-3xl lg:text-6xl">Getting Started</h1>
+            </div>
+            <p className="text-lg font-light text-neutral-300 lg:max-w-lg">
+              Lorem ipsum dolor sit amet, qui minim labore adipisicing minim
+              sint cillum sint consectetur cupidatat.
+            </p>
+          </div>
+          <div className="flex flex-col space-y-6">
+            {DOWNLOADS.map((item) => (
+              <div
+                key={"d-" + item.id}
+                className="p-6 flex flex-col bg-neutral-800 rounded-3xl border border-neutral-700 space-y-6"
+              >
+                <div className="flex flex-col space-y-3">
+                  <h2 className="text-xl font-light">{item.title}</h2>
+                  <p className="lg:text-lg font-light text-neutral-300">
+                    {item.description}
+                  </p>
+                </div>
+                <div className="flex flex-col space-y-3">
+                  {item.links.map((link) => (
+                    <Link
+                      key={"d-l-" + link.id}
+                      className="link"
+                      href={link.href}
+                      target="_blank"
+                    >
+                      {link.name}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/lib/data/downloads.ts
@@ -0,0 +1,37 @@
+export const DOWNLOADS = [
+  {
+    id: 1,
+    title: "Download and Implement the GATO Framework",
+    description: "This is the full, comprehensive GATO Framework.",
+    links: [
+      {
+        id: 1,
+        name: "GATO Framework PDF version 2023.05.21",
+        href: "https://drive.google.com/file/d/1pZhquKhaGwl5lrm2JR46cb5J_aiwlpQp/view?usp=share_link",
+      },
+      {
+        id: 2,
+        name: "GATO Framework DOCX version 2023.05.21",
+        href: "https://docs.google.com/document/d/1-qk449aZxx1c1TZOKTUDlhv5MU0HpvJk/edit?usp=share_link&ouid=100334453279403183774&rtpof=true&sd=true",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Download the GATO Handout",
+    description:
+      "This is a one- page overview of the GATO Framework for use in meetings, flyers, and so on.",
+    links: [
+      {
+        id: 1,
+        name: "GATO Handout PDF version 2023.05.21",
+        href: "https://drive.google.com/file/d/167TbSLTTD19kG9vM2q-EzzAASjLmCHXu/view?usp=share_link",
+      },
+      {
+        id: 2,
+        name: "GATO Handout PDF version 2023.05.21",
+        href: "https://docs.google.com/document/d/1w8iHtTVY6UbDxm1UoV9VlDSCiiqK4iHI/edit?usp=sharing&ouid=100334453279403183774&rtpof=true&sd=true",
+      },
+    ],
+  },
+];
