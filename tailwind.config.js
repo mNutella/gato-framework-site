64b56bd40f22aca985c3ab801a3711cd6ff7fdcc
nutella <netcore.click@gmail.com>
2023-06-16 12:21:49 -0400

    fix: tailwindcss nesting error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(50% 50% at 50% 50%, rgba(226, 232, 240, 0.1) 0%, rgba(226, 232, 240, 0) 100%);',
+        "gradient-radial":
+          "radial-gradient(50% 50% at 50% 50%, rgba(226, 232, 240, 0.1) 0%, rgba(226, 232, 240, 0) 100%);",
       },
     },
   },
   plugins: [],
-}
+};
